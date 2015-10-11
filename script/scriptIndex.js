60054c6c1d46765049178ba991b139ecaae47ab1
Alejandro Gómez <alejogom5@hotmail.com>
2015-10-11 14:43:37 -0500

    script
    commit

--- script/scriptIndex.js
+++ script/scriptIndex.js
@@ -1,31 +1,24 @@
 /**
  * Created by JUANPABLO on 11/10/2015.
  */
+dojo.ready(function() {
+    dojo.xhrGet({
+        url: "https://restcountries.eu/rest/v1/all",
+        handleAs: "json",
+        load: function (obj) {
+            dojo.forEach(obj, function (index, country) {
+                var countryObj = {
+                    name: country.name,
+                    capital: country.capital,
+                    alpha2Code: country.alpha2Code
+                };
+                /* here, obj will already be a JS object deserialized from the JSON response */
 
-dojo.xhrGet({
-    url: "https://restcountries.eu/rest/v1/all",
-    handleAs: "json",
-    load: function(obj) {
-        var countryObj = {
-            name: obj.name,
-            capital: obj.capital,
-            alpha2Code: obj.alpha2Code
-        };
-        /* here, obj will already be a JS object deserialized from the JSON response */
+                dojo.place('<label for="field">' + countryObj.name + '</label>');
 
-        require(["dojo/dom-construct"], function(domConstruct){
-            // create a div node
-            var node = domConstruct.create("div");
-        });
+            })
+        }
 
-
-
-
-
-    },
-    error: function(err) {
-        /* this will execute if the response couldn't be converted to a JS object,
-         or if the request was unsuccessful altogether. */
-    }
+    });
 });
 
