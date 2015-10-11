315c3d9e1952e3076d7f0356acca083a607e738b
jpaa07 <jparango93@gmail.com>
2015-10-11 13:12:45 -0500

    Web service

--- script/scriptIndex.js
+++ script/scriptIndex.js
@@ -1,3 +1,24 @@
 /**
  * Created by JUANPABLO on 11/10/2015.
  */
+
+// Create the store for later use
+var store = new dojo.data.ItemFileReadStore({
+    contentType: 'application/json'
+    ,clearOnClose: true
+    ,urlPreventCache: true
+    ,url: "http://192.168.1.65:9080/RAD8JAX-RSWeb/jaxrs/customers"
+});
+
+
+    store.fetch({
+        onItem: function(item, request) {
+            alert('I fire after each returned json item')
+        }
+        ,onComplete: function(items, findResult) {
+            alert('I fire when the data has loaded completely.');
+        }
+        ,onError: function(error, request) {
+            alert('I fire when an error occurs');
+        }
+    });
